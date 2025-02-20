b2ca4c722dc373a0884b6920fd167c2d7fc3131e
Rares Pasca <rares.pasca@reea.net>
2025-02-20 02:08:59 +0200

    add changes ConfigError: Config (unnamed): Key "env": This appears to be in eslintrc format rather than flat config format.

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,6 @@ import { Legacy } from '@eslint/eslintrc';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Import your custom rule
 import switchBreakAlign from './eslint-local-rules/switch-break-align.js';
 
 export default {
